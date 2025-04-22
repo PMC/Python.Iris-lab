544cdcd32623baabc7dbb438b23ddb671f8a4902
Pierre Chateau <pierre@chateau.se>
2025-04-22 20:47:53 +0200

    initial repo

---
+++ iris_training_lab.py
@@ -0,0 +1,57 @@
+from rich import print
+import keras
+from keras.layers import Input, Dense
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+import numpy as np
+
+
+data = pd.read_csv("iris.csv")
+
+# Separate features (X) and labels (y)
+X = data.drop(columns=["variety"])  # Assuming "variety" is the target column
+y = data["variety"]
+
+# Encode labels
+y = OneHotEncoder(sparse_output=False).fit_transform(y.values.reshape(-1, 1))
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+inputs = Input(shape=(4,), name="input")
+layer_x = Dense(6, activation="relu", name="dense_1")(inputs)
+layer_x = Dense(4, activation="relu", name="dense_2")(layer_x)
+outputs = Dense(3, activation="softmax", name="predictions")(layer_x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+# Compile the model
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+# Fit the model
+history = model.fit(X_train, y_train, epochs=200, validation_data=(X_test, y_test))
+
+model.save("iris_model_hot.keras")
+
+print("model.summary:")
+print(model.summary())
+print("-" * 30)
+
+result = model.evaluate(X_test, y_test)
+
+# slice test data to get the first x rows
+X_test_subset = X_test[:10]
+predictions = model.predict(X_test_subset)
+
+data_variety = np.array([["Setosa"], ["Versicolor"], ["Virginica"]])
+# Iterate through the predictions and get the expected variety
+for i in range(len(predictions)):
+    variety = data.iloc[X_test_subset.index[i]]["variety"]
+    indices = np.where(data_variety == variety)
+    print(
+        f"Sample {i + 1}: Predictions: {predictions[i]}, Expected: {indices[0][0]} - {variety}"
+    )
+
+
+print(f"Test loss: {result[0]} / Test accuracy: {result[1]}")
---
+++ main.py
@@ -0,0 +1,12 @@
+from rich import print
+import sys
+
+
+def main():
+    print("Hello from quadrunes-lab!")
+    print("Hello, [bold magenta]World[/bold magenta]!", ":vampire:", locals())
+    print(sys.version)
+
+
+if __name__ == "__main__":
+    main()
